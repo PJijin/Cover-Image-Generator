753df18f449b486e7b92f7bec19f3c636574af36
Pjijin <Pjijin1@gmail.com>
2019-08-11 11:15:45 +0530

    🙌🏻 Smooth draggable

--- src/components/draggable/draggable.component.jsx
+++ src/components/draggable/draggable.component.jsx
@@ -3,7 +3,7 @@ import Draggable from 'react-draggable';
 
 const DraggableComponent = ({ children }) => {
 	return (
-		<Draggable handle=".handle" defaultPosition={{ x: 0, y: 0 }} position={null} grid={[25, 25]} scale={1}>
+		<Draggable handle=".handle" defaultPosition={{ x: 0, y: 0 }} position={null} grid={[1, 1]} scale={1}>
 			<div>{children}</div>
 		</Draggable>
 	);
